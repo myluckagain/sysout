34733547ec2bd5ed90218a3995926fb322df4d70
Ekaterina Galkina <myluckagain@gmail.com>
2020-02-09 15:42:41 +0500

    fix pom

--- spring-core/src/main/java/ru/javalang/injection/Config.java
+++ spring-core/src/main/java/ru/javalang/injection/Config.java
@@ -1,6 +1,10 @@
 package ru.javalang.injection;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@ComponentScan
 public class Config {
 
 }
