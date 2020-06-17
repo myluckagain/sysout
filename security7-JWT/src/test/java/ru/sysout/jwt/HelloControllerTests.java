f5b25567cb39ce183133f06ac0e7d621ef9ba35d
myluckagain <myluckagain@gmail.com>
2020-06-17 23:19:36 +0500

    three tests added

--- security7-JWT/src/test/java/ru/sysout/jwt/HelloControllerTests.java
+++ security7-JWT/src/test/java/ru/sysout/jwt/HelloControllerTests.java
@@ -5,14 +5,12 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import ru.sysout.jwt.security.AuthRequest;
 import ru.sysout.jwt.security.AuthResponse;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith(SpringExtension.class)
@@ -27,7 +25,7 @@ public class HelloControllerTests {
         AuthResponse authResponse = getAuthHeaderForUser("user", "password");
 
         HttpHeaders headers = new HttpHeaders();
-        headers.add("Authorization", "Bearer "+authResponse.getJwtToken() );
+        headers.add("Authorization", "Bearer " + authResponse.getJwtToken());
 
         ResponseEntity<String> response = restTemplate.exchange("/user", HttpMethod.GET, new HttpEntity<>(headers), String.class);
 
@@ -35,9 +33,39 @@ public class HelloControllerTests {
 
     }
 
+    @Test
+    public void whenGetAdmin_thenCorrect() {
+
+
+        AuthResponse authResponse = getAuthHeaderForUser("admin", "password");
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "Bearer " + authResponse.getJwtToken());
+
+        ResponseEntity<String> response = restTemplate.exchange("/admin", HttpMethod.GET, new HttpEntity<>(headers), String.class);
+
+        assertTrue(response.getBody().equals("Admin"));
+
+    }
+
+    @Test
+    public void givenUserCredentials_whenGetAdmin_thenCorrect() {
+
+
+        AuthResponse authResponse = getAuthHeaderForUser("user", "password");
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "Bearer " + authResponse.getJwtToken());
+
+        ResponseEntity<String> response = restTemplate.exchange("/admin", HttpMethod.GET, new HttpEntity<>(headers), String.class);
+
+        assertEquals(response.getStatusCode(), HttpStatus.FORBIDDEN);
+
+    }
+
     private AuthResponse getAuthHeaderForUser(String name, String password) {
 
-        AuthRequest authRequest =new AuthRequest();
+        AuthRequest authRequest = new AuthRequest();
         authRequest.setName(name);
         authRequest.setPassword(password);
         AuthResponse authResponse = restTemplate.postForObject("/authenticate", authRequest, AuthResponse.class);
@@ -46,3 +74,4 @@ public class HelloControllerTests {
     }
 
 }
+
