c7596a53d19eb66d85ef8ff9c74aeb3a85e05735
myluckagain <myluckagain@gmail.com>
2020-11-06 00:33:25 +0500

    rename

--- batch-github/src/main/java/ru/sysout/dto/RepoRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.sysout.dto;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.sysout.model.Repo;
-
-public interface RepoRepository extends JpaRepository<Repo, Long> {
-}
