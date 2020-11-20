03b6caccaf032a932c0ea0eef3e54da73448f489
myluckagain <myluckagain@gmail.com>
2020-11-20 17:30:33 +0500

    fix

--- security1.1-custom-form/src/main/java/ru/sysout/security/SecurityConfig.java
+++ security1.1-custom-form/src/main/java/ru/sysout/security/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-   http.csrf().disable();
+  
         http.authorizeRequests()
                 .antMatchers("/**").hasRole("USER")
                 .and()
