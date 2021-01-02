e6bf1bc5e44c0c7899ef179b1d1c6e13cf974ad7
myluckagain <myluckagain@gmail.com>
2021-01-03 01:41:42 +0500

    refreshing config client

--- cloud3/zoo/src/main/java/ru/sysout/zoo/controller/ZooController.java
+++ cloud3/zoo/src/main/java/ru/sysout/zoo/controller/ZooController.java
@@ -2,12 +2,14 @@ package ru.sysout.zoo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.sysout.zoo.RandomAnimalClient;
 import ru.sysout.zoo.model.Animal;
 
 @RestController
+@RefreshScope
 public class ZooController {
     @Autowired
     private RandomAnimalClient randomAnimalClient;
