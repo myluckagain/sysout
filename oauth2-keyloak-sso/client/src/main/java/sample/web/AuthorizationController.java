9db2cc902082035cf576022999a5e3dbdedf65e0
myluckagain <myluckagain@gmail.com>
2021-07-10 01:41:53 +0500

    sso with keyloak

---
+++ oauth2-keyloak-sso/client/src/main/java/sample/web/AuthorizationController.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package sample.web;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.OAuth2Error;
+import org.springframework.security.oauth2.core.endpoint.OAuth2ParameterNames;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import javax.servlet.http.HttpServletRequest;
+
+
+@Controller
+public class AuthorizationController {
+    private final WebClient webClient;
+    private final String messagesBaseUri;
+
+    public AuthorizationController(WebClient webClient,
+                                   @Value("${messages.base-uri}") String messagesBaseUri) {
+        this.webClient = webClient;
+        this.messagesBaseUri = messagesBaseUri;
+    }
+
+    @GetMapping(value = "/remotemessages")
+    public String authorizationCodeGrant(Model model,
+                                         @AuthenticationPrincipal OAuth2User oauth2User) {
+        String[] messages = this.webClient
+                .get()
+                .uri(this.messagesBaseUri)
+                .retrieve()
+                .bodyToMono(String[].class)
+                .block();
+        model.addAttribute("messages", messages);
+        model.addAttribute("username", oauth2User.getName());
+        return "remotemessages";
+    }
+
+    // '/authorized' is the registered 'redirect_uri' for authorization_code
+    @GetMapping(value = "/authorized", params = OAuth2ParameterNames.ERROR)
+    public String authorizationFailed(Model model, HttpServletRequest request) {
+        String errorCode = request.getParameter(OAuth2ParameterNames.ERROR);
+        if (StringUtils.hasText(errorCode)) {
+            model.addAttribute("error",
+                    new OAuth2Error(
+                            errorCode,
+                            request.getParameter(OAuth2ParameterNames.ERROR_DESCRIPTION),
+                            request.getParameter(OAuth2ParameterNames.ERROR_URI))
+            );
+        }
+        return "index";
+    }
+}
